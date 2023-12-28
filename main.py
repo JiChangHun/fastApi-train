95521a96b1c0441646e85f4ffe45d83f66a6643c
JiChangHun <gch3332@gmail.com>
2023-12-28 09:14:33 +0900

    create, get(id), delete
    create post, get post with id, delete post

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 from typing import Optional
-from fastapi import FastAPI
+from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel
+from random import randrange
 
 app = FastAPI()
 
@@ -12,16 +13,44 @@ class Post(BaseModel):
     rating: int = None
 
 
+my_posts = [{"title": "title of post 1", "content": "content of post 1", "id": 1},
+            {"title": "favorite foods", "content": "I like pizza", "id": 2}]
+
+def find_post(id):
+    for i, post in enumerate(my_posts):
+        print(i, post)
+        if post["id"] == id:
+            return i, post
+    return None, None
+
 @app.get("/")
 def root():
     return {"message": "Hello World!"}
 
 @app.get("/posts")
 def get_posts():
-    return {"data": "This is your posts"}
+    return {"data": my_posts}
 
-@app.post("/createposts")
-def create_posts(payload: Post):
-    print(payload.rating)
-    return {"data": "new post"}
-# title: str, content: str
+@app.post("/posts", status_code= status.HTTP_201_CREATED)
+def create_posts(post: Post):
+    post_dict = post.model_dump()
+    post_dict['id'] = randrange(3, 1000000)
+    my_posts.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id: int):
+    _, post = find_post(id)
+    if not post:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail= f"post with id: {id} was not found")
+    return {"post_detail": post}
+
+@app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    idx, _ = find_post(id)
+    if idx is None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail= f"post with id: {id} was not found")
+    my_posts.pop(idx)
+    return Response(status_code= status.HTTP_204_NO_CONTENT)
