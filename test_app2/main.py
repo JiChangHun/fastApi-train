a2a17f1f41d735c6cf4291c6e0f3c2453b5c8f29
JiChangHun <gch3332@gmail.com>
2024-01-09 09:08:54 +0900

    test_app2

---
+++ test_app2/main.py
@@ -0,0 +1,76 @@
+from fastapi import FastAPI, Response, status, HTTPException, Depends
+from sqlalchemy.orm import Session
+
+import models, schemas, utils
+from database import engine, get_db
+
+from typing import List
+
+models.Base.metadata.create_all(bind= engine)
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "Hello World!"}
+
+@app.get("/posts", response_model= List[schemas.Post])
+def get_posts(db: Session = Depends(get_db)):
+    posts = db.query(models.Post).all()
+    return posts
+
+@app.post("/posts", status_code= status.HTTP_201_CREATED, response_model= schemas.Post)
+def create_posts(post: schemas.PostCreate, db: Session = Depends(get_db)):
+    new_post = models.Post(**post.model_dump())
+    db.add(new_post)
+    db.commit()
+    db.refresh(new_post)
+    return new_post
+
+@app.get("/posts/{id}", response_model= schemas.Post)
+def get_post(id: int, db: Session = Depends(get_db)):
+    post = db.query(models.Post).filter(models.Post.id == id).first()
+    if not post:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail= f"post with id: {id} was not found")
+    return post
+
+@app.delete("/posts/{id}", status_code= status.HTTP_204_NO_CONTENT)
+def delete_post(id: int, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    if post_query.first() is None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail= f"post with id: {id} was not found")
+    else:
+        post_query.delete(synchronize_session= False)
+        db.commit()
+    return Response(status_code= status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}", response_model= schemas.Post)
+def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db)):
+    post_query = db.query(models.Post).filter(models.Post.id == id)
+    if post_query.first() is None:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail= f"post with id: {id} was not found")
+    else:
+        post_query.update(post.model_dump(), synchronize_session= False)
+        db.commit()
+    return post_query.first()
+
+@app.post("/users", status_code= status.HTTP_201_CREATED, response_model= schemas.UserOut)
+def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
+    user.password = utils.hash(user.password)
+    new_user = models.User(**user.model_dump())
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+
+    return new_user
+
+@app.get("/users/{id}", response_model= schemas.UserOut)
+def get_user(id: int, db: Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.id == id).first()
+    if not user:
+        raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
+                            detail= f"user with id: {id} does not exist")
+    return user
---
+++ test_app2/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import Column, Integer, String, Boolean, TIMESTAMP, text
+from database import Base
+
+class Post(Base):
+    __tablename__ = "posts"
+
+    id = Column(Integer, nullable= False, primary_key= True)
+    title = Column(String, nullable= False)
+    content = Column(String, nullable= False)
+    published = Column(Boolean, nullable= False, server_default= 'TRUE')
+    created_at = Column(TIMESTAMP(timezone= True), nullable= False, 
+                        server_default= text('now()'))
+
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, nullable= False, primary_key= True)
+    email = Column(String, nullable= False, unique= True)
+    password = Column(String, nullable= False)
+    created_at = Column(TIMESTAMP(timezone= True), nullable= False,
+                        server_default= text('now()'))
