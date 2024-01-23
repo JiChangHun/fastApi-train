e4edde00e1377932f6b6786de5d3f1590502342c
JiChangHun <gch3332@gmail.com>
2024-01-23 16:23:09 +0900

    add cascade in foreignkey, auth in (delete, update)

--- app/models.py
+++ app/models.py
@@ -5,7 +5,7 @@ class Post(Base):
     __tablename__ = "posts"
 
     id = Column(Integer, primary_key= True, nullable= False)
-    user_id = Column(Integer, ForeignKey("users.id"), nullable= False)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete= "CASCADE"), nullable= False)
     title = Column(String, nullable= False)
     content = Column(String, nullable= False)
     published = Column(Boolean, nullable= False, server_default= 'TRUE')
--- app/routers/post.py
+++ app/routers/post.py
@@ -12,7 +12,7 @@ router = APIRouter(
 
 @router.get("", response_model= List[schemas.Post])
 def get_posts(db: Session = Depends(get_db), 
-            user_id: str = Depends(oauth2.get_current_user)):
+            current_user: str = Depends(oauth2.get_current_user)):
     posts = db.query(models.Post).all()
     return posts
 
@@ -21,8 +21,7 @@ def create_posts(post: schemas.PostCreate,
                 db: Session = Depends(get_db),
                 current_user: str = Depends(oauth2.get_current_user)):
 
-    new_post = models.Post(**post.model_dump())
-    new_post.user_id = current_user.id
+    new_post = models.Post(user_id= current_user.id, **post.model_dump())
     
     db.add(new_post)
     db.commit()
@@ -42,9 +41,14 @@ def get_post(id: int, db: Session = Depends(get_db),
 def delete_post(id: int, db: Session = Depends(get_db), 
                 current_user: str = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
-    if post_query.first() is None:
+    post = post_query.first()
+
+    if not post:
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
                             detail= f"post with id: {id} was not found")
+    elif post.user_id != current_user.id:
+        raise HTTPException(status_code= status.HTTP_403_FORBIDDEN,
+                            detail= "Not authorized to perform requested action")
     else:
         post_query.delete(synchronize_session= False)
         db.commit()
@@ -54,9 +58,14 @@ def delete_post(id: int, db: Session = Depends(get_db),
 def update_post(id: int, post: schemas.PostCreate, db: Session = Depends(get_db),
                 current_user: str = Depends(oauth2.get_current_user)):
     post_query = db.query(models.Post).filter(models.Post.id == id)
-    if post_query.first() is None:
+    post = post_query.first()
+
+    if post is None:
         raise HTTPException(status_code= status.HTTP_404_NOT_FOUND,
                             detail= f"post with id: {id} was not found")
+    elif post.user_id != current_user.id:
+        raise HTTPException(status_code= status.HTTP_403_FORBIDDEN,
+                            detail= "Not authorized to perform requested action")
     else:
         post_query.update(post.model_dump(), synchronize_session= False)
         db.commit()
